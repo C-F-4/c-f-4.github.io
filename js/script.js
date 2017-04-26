e39dd4884009735241fa706923b3d741becf974f
cfx.404 <C-F-4@users.noreply.github.com>
2017-04-26 18:42:31 +0530

    Updated Styling for smoke class

--- js/script.js
+++ js/script.js
@@ -1,2 +1,12 @@
 (function() {
+  $(document).ready(function() {
+	  $('.smoke').funnyText({
+		  speed: 700,
+		  borderColor: 'black',
+		  activeColor: 'white',
+		  color: 'black',
+		  fontSize: '7em',
+		  direction: 'both'
+	  });
+  });
 })();
